1cd03de91be655b28c05b9543bc3f4b090c0dc19
SunQthecodemaker <sunq818@gmail.com>
2026-04-22 17:48:14 +0900

    chore: 캐시 버스터 일괄 업데이트 v20260422a (권한 버튼 fix 반영)

--- main.v2.js
+++ main.v2.js
@@ -1,9 +1,9 @@
 import { state, db } from './state.js';
 import { _, _all, show, hide } from './utils.js';
-import { renderScheduleManagement } from './schedule.js?v=20260417a';
-import { assignManagementEventHandlers, getManagementHTML, getDepartmentManagementHTML, getLeaveListHTML, getLeaveManagementHTML, handleBulkRegister, getLeaveStatusHTML, addLeaveStatusEventListeners } from './management.js?v=20260417a';
-import { renderDocumentReviewTab, renderTemplatesManagement } from './documents.js?v=20260417a';
-import { renderEmployeePortal } from './employee-portal-final.js?v=20260417c';
+import { renderScheduleManagement } from './schedule.js?v=20260422a';
+import { assignManagementEventHandlers, getManagementHTML, getDepartmentManagementHTML, getLeaveListHTML, getLeaveManagementHTML, handleBulkRegister, getLeaveStatusHTML, addLeaveStatusEventListeners } from './management.js?v=20260422a';
+import { renderDocumentReviewTab, renderTemplatesManagement } from './documents.js?v=20260422a';
+import { renderEmployeePortal } from './employee-portal-final.js?v=20260422a';
 import { getLeaveDetails } from './leave-utils.js';
 
 // Safely initialize dayjs plugins
